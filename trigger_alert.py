99dfb4b3a02d748049bf283be5442c31772f952a
SSA <soesanaung2147@gmail.com>
2024-06-16 22:44:39 +0800

    0416062024

--- trigger_alert.py
+++ trigger_alert.py
@@ -2,12 +2,12 @@ import os
 import time
 
 ip_address = "192.168.11.199"
-timeout = 10  # seconds
+timeout = 300  # seconds
 
 while True:
     response = os.system("ping -c 1 " + ip_address)
     if response!= 0:
         print("No response from", ip_address)
-        os.system("systemctl stop datasync.service")
+        os.system("systemctl stop datasyncn.service")
         break
     time.sleep(timeout)
